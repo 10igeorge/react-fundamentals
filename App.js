bb6873f506dd0a2105d9ef8a63c6019de75e3645
10igeorge <10izzygeorge@gmail.com>
2016-06-04 20:33:06 -0700

    live update page with user input

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {txt: 'this is the state text'}
+	}
+	update(e){
+		this.setState({txt: e.target.value})
+	}
 	render(){
-		let txt = this.props.txt
-		return <h1>{txt}</h1>
+		return (
+			<div>
+			<input type="text" onChange={this.update.bind(this)}/>
+			<h1>{this.state.txt}</h1>
+			</div>
+		)
 	}
 }
 
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	cat: 5
-}
-
-ReactDOM.render(
-	<App txt="this is the props text" />,
-	document.getElementById('app')
-);
+export default App
